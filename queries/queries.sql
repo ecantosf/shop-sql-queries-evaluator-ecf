c40477de7f1ef1825c9fb928be84e8a5b49b1739
ecantosf <eduard.cantos@gmail.com>
2026-01-29 22:55:14 +0100

    feat: round product prices to nearest integer

--- queries/queries.sql
+++ queries/queries.sql
@@ -31,7 +31,8 @@ SELECT nombre, UPPER(LEFT(nombre, 2)) AS iniciales
 FROM fabricante;
 
 -- 9. Llista els noms i els preus dels productes, arrodonint el valor del preu (precio)
-
+SELECT nombre, ROUND(precio) AS precio
+FROM producto;
 
 -- 10. Llista els noms i els preus de tots els productes (precio truncado) de la taula producto, truncant el valor del preu per a mostrar-lo sense cap xifra decimal.
 
