e1a328db049d59185c0863c400227b9e16fd47ed
ecantosf <eduard.cantos@gmail.com>
2026-01-30 10:28:39 +0100

    feat: add query for products from specific manufacturers

--- queries/queries.sql
+++ queries/queries.sql
@@ -151,7 +151,13 @@ JOIN fabricante f ON p.codigo_fabricante = f.codigo
 WHERE f.nombre = 'Crucial' AND p.precio > 200;
 
 -- 28. Retorna un llistat amb nom, preu i nom del fabricant (fabricante) de tots els productes dels fabricants Asus, Hewlett-Packard i Seagate. Sense utilitzar l'operador IN.
-
+SELECT
+    p.nombre,
+    p.precio,
+    f.nombre AS fabricante
+FROM producto p
+JOIN fabricante f ON p.codigo_fabricante = f.codigo
+WHERE f.nombre = 'Asus' OR f.nombre = 'Hewlett-Packard' OR f.nombre = 'Seagate';
 
 -- 29. Retorna un llistat amb nom, preu i nom del fabricant (fabricante) de tots els productes dels fabricants Asus, Hewlett-Packard i Seagate. Fent servir l'operador IN.
 
