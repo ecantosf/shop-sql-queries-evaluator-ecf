ac3900dda08003645c8f1ec3bc6b31bfe39ef44e
ecantosf <eduard.cantos@gmail.com>
2026-01-30 10:36:51 +0100

    Add SQL query for products with manufacturer name ending in 'e'

--- queries/queries.sql
+++ queries/queries.sql
@@ -169,7 +169,13 @@ JOIN fabricante f ON p.codigo_fabricante = f.codigo
 WHERE f.nombre IN('Asus', 'Hewlett-Packard', 'Seagate');
 
 -- 30. Retorna un llistat amb el nom, el preu i el nom del fabricant (fabricante) dels productes, on el nom del fabricant acabi en la lletra 'e'.
-
+SELECT
+    p.nombre,
+    p.precio,
+    f.nombre AS fabricante
+FROM producto p
+JOIN fabricante f ON p.codigo_fabricante = f.codigo
+WHERE f.nombre LIKE '%e';
 
 -- 31. Retorna un llistat amb el nom del producte, el seu preu i el nom del fabricant (fabricante), per a tots aquells productes els fabricants dels quals contenen la lletra 'w' en el seu nom.
 
