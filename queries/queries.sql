89e89cf7bb786648d06b694e4ca7a3d2ed65ff4e
ecantosf <eduard.cantos@gmail.com>
2026-01-30 10:04:36 +0100

    Fix typo in SQL query for manufacturer name

--- queries/queries.sql
+++ queries/queries.sql
@@ -118,7 +118,7 @@ JOIN fabricante f ON p.codigo_fabricante = f.codigo;
 SELECT
     p.nombre,
     p.precio,
-    f.nombre AS fabricante
+    f.nombre AS fabricant
 FROM producto p
 JOIN fabricante f ON p.codigo_fabricante = f.codigo
 ORDER BY p.precio
